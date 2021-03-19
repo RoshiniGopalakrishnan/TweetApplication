469e22fccc1f5d50cbc04dfa8584612031939783
roshinigopalakrishnan <roshinigkrishnan@gmail.com>
2021-03-19 19:29:38 +0530

    Tweet Application

--- tweet_app.sql
+++ tweet_app.sql
@@ -37,4 +37,11 @@ CREATE TABLE IF NOT EXISTS `tweetapp`.`tweet` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
+INSERT INTO user  (`us_first_name`, `us_last_name`, `us_gender`, `us_date_of_birth`, `us_email_id`, `us_password`, `us_isvalid`)
+ VALUES ('Roshini', 'Gopalakrishnan', 'Female', '1997-01-07', 'roshini@gmail.com', 'password', '1'),
+ ('Helen', 'Mathew', 'Female', '1993-08-07', 'helen@gmail.com', 'password', '1'),
+ ('Anu', 'Micheal', 'Female', '1947-09-26', 'anu@gmail.com', 'anu', '0');
+ 
+ INSERT INTO tweet(tweet_msg,user_email_id)values('Hello welcome to tweet app!','roshini@gmail.com'),
+ ('trending tweets','anu@gmail.com'),
+ ('congrats!! @kiran','helen@gmail.com');
